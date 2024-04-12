4d72e8c7f7df92e0a5a2793eaa929abb21403508
Rick Zhang <rickzhang@live.ca>
2024-04-12 17:36:13 -0400

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-    js.configs.recommended,
-   {
-       rules: {
-           "no-unused-vars": "warn",
-           "no-undef": "warn"
-       }
-   }
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
 ];
