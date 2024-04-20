046169757cf44bf56c6a7a663446cbd3340e53df
Rick Zhang <rick@thenarrative.dev>
2024-04-20 09:23:49 -0700

    Add scopes file

---
+++ src/constants/scopes.ts
@@ -0,0 +1,10 @@
+export const scopes = [
+  'channel:bot',
+  'channel:manage:broadcast',
+  'channel:manage:predictions',
+  'channel:moderate',
+  'chat:edit',
+  'chat:read',
+  'moderator:manage:banned_users',
+  'user:bot',
+];
