30c80258abf0e2aa090e0abe613fde6bf99c07fa
Rick Zhang <rick@thenarrative.dev>
2024-07-05 15:47:08 -0400

    Add tests for checkSpam

--- src/utils/checkSpam.test.ts
+++ src/utils/checkSpam.test.ts
@@ -1,5 +1,20 @@
-import { describe, expect, it } from '@jest/globals';
-import { isExempt } from './checkSpam';
+import { afterAll, beforeAll, describe, expect, it, jest } from '@jest/globals';
+import { HelixChannelFollower } from '@twurple/api';
+import { CheckSpamInput, checkSpam, isExempt } from './checkSpam';
+
+// jest.mock('@twurple/chat', () => ({
+//   ChatMessage: jest.fn().mockImplementation(() => {
+//     const badges = new Map<string, string>();
+//     badges.set('vip', '1');
+
+//     return {
+//       userInfo: {
+//         badges,
+//       },
+//       isFirst: false,
+//     };
+//   }),
+// }));
 
 describe('spam', () => {
   describe('isExempt', () => {
@@ -16,6 +31,174 @@ describe('spam', () => {
     describe('returns false', () => {
       it('if none of the badges are in the exemption list', () => {
         expect(isExempt(['glhf-pledge', 'subscriber'])).toBeFalsy();
+      });
+    });
+  });
+
+  describe('checkSpam', () => {
+    describe('with an exempt badge', () => {
+      beforeAll(() => {
+        jest.doMock('@twurple/chat', () => ({
+          ChatMessage: jest.fn().mockImplementation(() => {
+            const badges = new Map<string, string>();
+            badges.set('vip', '1');
+
+            return {
+              userInfo: {
+                badges,
+              },
+              isFirst: false,
+            };
+          }),
+        }));
+      });
+
+      afterAll(() => {
+        jest.resetModules();
+      });
+
+      it('should return false', () => {
+        const { ChatMessage } = require('@twurple/chat');
+
+        const followerData = [];
+        const message = new ChatMessage('command', {
+          prefix: { nick: 'rickstergg' },
+        });
+
+        const input: CheckSpamInput = {
+          followerData,
+          message,
+        };
+
+        expect(checkSpam(input)).toBeFalsy();
+      });
+    });
+
+    describe('with no exemptions', () => {
+      describe('is not following', () => {
+        beforeAll(() => {
+          jest.doMock('@twurple/chat', () => ({
+            ChatMessage: jest.fn().mockImplementation(() => {
+              const badges = new Map<string, string>();
+              badges.set('subscriber', '33');
+              badges.set('glhf-pledge', '1');
+
+              return {
+                userInfo: {
+                  badges,
+                },
+                isFirst: false,
+              };
+            }),
+          }));
+        });
+
+        afterAll(() => {
+          jest.resetModules();
+        });
+
+        it('should return true', () => {
+          const { ChatMessage } = require('@twurple/chat');
+
+          const followerData = [];
+
+          const message = new ChatMessage('command', {
+            prefix: { nick: 'rickstergg' },
+          });
+
+          const input: CheckSpamInput = {
+            followerData,
+            message,
+          };
+
+          expect(checkSpam(input)).toBeTruthy();
+        });
+      });
+
+      describe('has recently followed', () => {
+        beforeAll(() => {
+          jest.doMock('@twurple/chat', () => ({
+            ChatMessage: jest.fn().mockImplementation(() => {
+              const badges = new Map<string, string>();
+              badges.set('subscriber', '33');
+              badges.set('glhf-pledge', '1');
+
+              return {
+                userInfo: {
+                  badges,
+                },
+                isFirst: false,
+              };
+            }),
+          }));
+        });
+
+        afterAll(() => {
+          jest.resetModules();
+        });
+
+        it('should return true', () => {
+          const { ChatMessage } = require('@twurple/chat');
+
+          const followerData = [
+            new HelixChannelFollower({
+              user_id: '123',
+              user_login: 'asdf',
+              user_name: 'asdf',
+              followed_at: new Date(new Date().getTime()).toString(),
+            }),
+          ];
+
+          const message = new ChatMessage('command', {
+            prefix: { nick: 'rickstergg' },
+          });
+
+          const input: CheckSpamInput = {
+            followerData,
+            message,
+          };
+
+          expect(checkSpam(input)).toBeTruthy();
+        });
+      });
+
+      describe('sent first message', () => {
+        beforeAll(() => {
+          jest.doMock('@twurple/chat', () => ({
+            ChatMessage: jest.fn().mockImplementation(() => {
+              const badges = new Map<string, string>();
+              badges.set('subscriber', '33');
+              badges.set('glhf-pledge', '1');
+
+              return {
+                userInfo: {
+                  badges,
+                },
+                isFirst: true,
+              };
+            }),
+          }));
+        });
+
+        afterAll(() => {
+          jest.resetModules();
+        });
+
+        it('should return true', () => {
+          const { ChatMessage } = require('@twurple/chat');
+          const followerData = [];
+
+          const message = new ChatMessage('command', {
+            prefix: { nick: 'rickstergg' },
+          });
+
+          const input: CheckSpamInput = {
+            followerData,
+            message,
+          };
+
+          expect(checkSpam(input)).toBeTruthy();
+        });
       });
     });
   });
