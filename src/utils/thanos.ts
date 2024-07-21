fd4f45daba6f89eac96c9d9b7bed24dac44b27a6
Rick Zhang <rick@thenarrative.dev>
2024-07-21 19:43:45 -0400

    Add some more thanos quotes

--- src/utils/thanos.ts
+++ src/utils/thanos.ts
@@ -4,6 +4,9 @@ export const quotes = [
   "When I'm done, half of humanity will still be alive.",
   "You should've gone for the head.",
   'I am inevitable.',
+  'Is that sadness I sense in you, daughter?',
+  "The Tesseract or your brother's head.",
+  'All that for a drop of blood.',
 ];
 
 export const shuffleChatters = (
