0a3540c23298934a56df1fe87e537dcfaafce611
Rick Zhang <13734404+rickstergg@users.noreply.github.com>
2025-01-04 10:23:15 -0500

    Fix up some tests

--- src/shoutouts.test.ts
+++ src/shoutouts.test.ts
@@ -19,7 +19,7 @@ describe('shoutoutManager', () => {
   describe('initialization', () => {
     it('should read the streamer path file', async () => {
       const manager = new Shoutouts();
-      await manager.initialize();
+      await manager.initialize('rickstergg');
 
       expect(fs.readFile).toBeCalledWith(streamerPath, 'utf-8');
       expect(manager.streamerList).toHaveLength(3);
@@ -31,34 +31,39 @@ describe('shoutoutManager', () => {
 
     beforeAll(async () => {
       manager = new Shoutouts();
-      await manager.initialize();
+      await manager.initialize('rickstergg');
     });
+
+    it('should actively exclude the streamer from the list if it exists', () => {
+      expect(manager.streamersToShoutout.has('rickstergg')).toBeFalsy();
+      expect(manager.streamersToShoutout.size).toBe(2);
+    })
 
     it('should return false for a streamer not in the list', () => {
       expect(manager.shouldShoutOut('not a streamer')).toBeFalsy();
     });
 
     it('should return true for a streamer in the list the first time', () => {
-      expect(manager.shouldShoutOut('rickstergg')).toBeTruthy();
-      expect(manager.streamersToShoutout.size).toBe(2);
+      expect(manager.shouldShoutOut('qqobes33')).toBeTruthy();
+      expect(manager.streamersToShoutout.size).toBe(1);
     });
 
     it('should return false for a streamer already shoutted out', () => {
-      expect(manager.shouldShoutOut('rickstergg')).toBeFalsy();
+      expect(manager.shouldShoutOut('qqobes33')).toBeFalsy();
     });
   });
 
   describe('reset', () => {
     it('should reset the streamer list after reading', async () => {
       const manager = new Shoutouts();
-      await manager.initialize();
+      await manager.initialize('rickstergg');
 
-      expect(manager.streamersToShoutout.size).toBe(3);
-      manager.shouldShoutOut('rickstergg');
       expect(manager.streamersToShoutout.size).toBe(2);
+      manager.shouldShoutOut('qqobes33');
+      expect(manager.streamersToShoutout.size).toBe(1);
 
       manager.reset();
-      expect(manager.streamersToShoutout.size).toBe(3);
+      expect(manager.streamersToShoutout.size).toBe(2);
     });
   });
 });
