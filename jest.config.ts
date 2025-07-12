20c89cbf8f726cacba9db548f0de1ce8109d371a
Rick Zhang <13734404+rickstergg@users.noreply.github.com>
2025-07-12 12:18:49 -0400

    Add nodes types, fixing CI

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,8 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
+
+export default config;
